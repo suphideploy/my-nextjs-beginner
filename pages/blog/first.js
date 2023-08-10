8e3a532594a1b8f61530b44ca4fa224bf24f0ab3
suphideploy <chingchimplee@gmail.com>
2023-08-10 16:12:44 +0700

    Create Router mit Verschieden Art und Weise

---
+++ pages/blog/first.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const first = () => {
+  return (
+    <div>first blog</div>
+  )
+}
+
+export default first
---
+++ pages/blog/second.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const second = () => {
+  return (
+    <div>second blog</div>
+  )
+}
+
+export default second
