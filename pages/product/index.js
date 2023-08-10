704759900c8afa65fc7a18cacded8eebf2fba8bc
suphideploy <chingchimplee@gmail.com>
2023-08-10 17:06:31 +0700

    LinkNavi/404Page

--- pages/product/index.js
+++ pages/product/index.js
@@ -1,9 +1,24 @@
-function ProductList() {
+import Link from 'next/link'
+function ProductList({productId = 100}) {
     return (
         <>
-        <h2>Product 1</h2>
-        <h2>Product 2</h2>
-        <h2>Product 3</h2>
+         <Link href='/'>
+            Home
+         </Link>
+        <h2>
+           <Link href="/product/1">Product 1</Link>
+        </h2>
+        <h2>
+           <Link href="/product/2">Product 2</Link>
+        </h2>
+        <h2>
+           <Link href="/product/3">Product 3</Link>
+        </h2>
+        <h2>
+        <Link href={`product/${productId}`}>
+          Product {productId}
+      </Link>
+      </h2>
         </>
     )
 }
